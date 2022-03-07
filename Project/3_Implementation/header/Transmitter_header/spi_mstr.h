da7867a02f436310a0b0e65930ff1dd41f818eba
Kowsik15 <98818618+Kowsik15@users.noreply.github.com>
2022-03-07 12:13:53 +0530

    Update spi_mstr.h

--- Project/3_Implementation/header/Transmitter_header/spi_mstr.h
+++ Project/3_Implementation/header/Transmitter_header/spi_mstr.h
@@ -1,1 +1,21 @@
-
+void SPImstr_init()
+{
+	DDRB |= (1<<PB4)|(1<<PB5)|(1<<PB7);
+	DDRB &= ~(1<<PB6);	
+	PORTB |= (1<<PB4);	
+	SPCR = (1<<SPE)|(1<<MSTR)|(1<<SPR0);	
+	SPSR &= ~(1<<SPI2X);	
+}
+void SPI_write(char data)
+{
+	char flush_buffer;
+	SPDR=data;
+	while (!(SPSR & (1<<SPIF)));
+	flush_buffer=SPDR;
+}
+char SPI_read()
+{
+	SPDR=0xFF;
+	while(!(SPSR & (1<<SPIF)));
+	return(SPDR);
+}
